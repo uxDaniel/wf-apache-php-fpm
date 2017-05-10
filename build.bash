fa5760f272ec56b0a553011737210768053d1788
Ryan Sanden <ryan@ryansanden.com>
2017-05-10 10:50:21 +0800

    add httpd to build script

--- build.bash
+++ build.bash
@@ -3,22 +3,66 @@
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$MYDIR"
 
+mkdir -p "$MYDIR/src"
+
 # PHP Version
 PHPVER=7.1.4
 
 # Install Prefix
 PREFIX="$HOME/shared/php-${PHPVER}"
+mkdir -p $PREFIX
 
-mkdir -p src
 
-#Install php
+###########################################################
+# PHP 7.1.4
+# original: http://us1.php.net/get/php-7.1.4.tar.xz
+###########################################################
 cd "$MYDIR/src"
 wget "http://us1.php.net/get/php-${PHPVER}.tar.xz/from/this/mirror" -O php-${PHPVER}.tar.xz
 tar -xf php-${PHPVER}.tar.xz
 cd php-${PHPVER}
 
-mkdir -p $PREFIX
-./configure --prefix=$PREFIX --with-libdir=lib64 --enable-fpm --with-pdo-mysql --with-mysqli --with-pdo-pgsql --with-pgsql --enable-bcmath --enable-calendar --enable-exif --enable-ftp --enable-mbstring --enable-soap --enable-zip --with-curl --with-freetype-dir --with-gd --with-gettext --with-gmp --with-iconv --with-imap --with-imap-ssl --with-jpeg-dir --with-kerberos --with-ldap --with-mcrypt --with-mhash --with-openssl --with-png-dir --with-pspell --with-tidy --with-xmlrpc --with-xsl --with-zlib-dir --without-pear --enable-sockets --enable-intl --with-webp-dir --enable-pcntl --with-mysql-sock=/var/lib/mysql/mysql.sock
+./configure --prefix="$PREFIX" --with-libdir=lib64 --enable-fpm --with-pdo-mysql --with-mysqli --with-pdo-pgsql --with-pgsql --enable-bcmath --enable-calendar --enable-exif --enable-ftp --enable-mbstring --enable-soap --enable-zip --with-curl --with-freetype-dir --with-gd --with-gettext --with-gmp --with-iconv --with-imap --with-imap-ssl --with-jpeg-dir --with-kerberos --with-ldap --with-mcrypt --with-mhash --with-openssl --with-png-dir --with-pspell --with-tidy --with-xmlrpc --with-xsl --with-zlib-dir --without-pear --enable-sockets --enable-intl --with-webp-dir --enable-pcntl --with-mysql-sock=/var/lib/mysql/mysql.sock
 
-time make -j8
+make -j8
+make install
+
+###########################################################
+# APR 1.5.2
+# original: http://apache.communilink.net/apr/apr-1.5.2.tar.bz2
+###########################################################
+cd "$MYDIR/src"
+wget 'http://apache.communilink.net/apr/apr-1.5.2.tar.bz2'
+tar -xf apr-1.5.2.tar.bz2
+cd apr-1.5.2
+./configure --prefix="$PREFIX"
+make -j8
+make install
+
+###########################################################
+# APR-Util 1.5.4
+# original: http://apache.communilink.net/apr/apr-util-1.5.4.tar.bz2
+###########################################################
+cd "$MYDIR/src"
+wget 'http://apache.communilink.net/apr/apr-util-1.5.4.tar.bz2'
+tar -xf apr-util-1.5.4.tar.bz2
+cd apr-util-1.5.4
+./configure --prefix="$PREFIX" --with-apr="$PREFIX"
+make -j8
+make install
+
+###########################################################
+# Apache 2.4.25
+# original: http://apache.communilink.net//httpd/httpd-2.4.25.tar.bz2
+###########################################################
+cd "$MYDIR/src"
+wget 'http://apache.communilink.net//httpd/httpd-2.4.25.tar.bz2'
+tar -xf httpd-2.4.25.tar.bz2
+cd httpd-2.4.25
+./configure --prefix="$PREFIX" \
+            --enable-mods-shared=all \
+            --enable-mpms-shared=all \
+            --with-apr="$PREFIX" \
+            --with-apr-util="$PREFIX"
+make -j8
 make install
