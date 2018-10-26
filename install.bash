c7bd72bd9a4e7cb8b820c493fce42dbac99a51f5
Ryan Sanden <ryan@ryansanden.com>
2018-10-26 21:15:20 +0800

    update to apache 2.4.37 and php 7.2.11

--- install.bash
+++ install.bash
@@ -8,56 +8,50 @@ source "$MYDIR/config"
 mkdir -p "$PREFIX/src"
 
 ###########################################################
-# PHP 7.1.4
-# original: http://us1.php.net/get/php-7.1.4.tar.xz
+# PHP 7.2.11
+# original: http://us1.php.net/get/php-7.2.11.tar.xz
 ###########################################################
 cd "$PREFIX/src"
-wget 'http://mirror.ryansanden.com/phpfpm-7.1.4/php-7.1.4.tar.xz'
-tar -xf php-7.1.4.tar.xz
-cd php-7.1.4
-
-./configure --prefix="$PREFIX" --with-libdir=lib64 --enable-fpm --with-pdo-mysql --with-mysqli --with-pdo-pgsql --with-pgsql --enable-bcmath --enable-calendar --enable-exif --enable-ftp --enable-mbstring --enable-soap --enable-zip --with-curl --with-freetype-dir --with-gd --with-gettext --with-gmp --with-iconv --with-imap --with-imap-ssl --with-jpeg-dir --with-kerberos --with-ldap --with-mcrypt --with-mhash --with-openssl --with-png-dir --with-pspell --with-tidy --with-xmlrpc --with-xsl --with-zlib-dir --without-pear --enable-sockets --enable-intl --with-webp-dir --enable-pcntl --with-mysql-sock=/var/lib/mysql/mysql.sock
-
+wget 'http://mirror.ryansanden.com/phpfpm-7.2.11/php-7.2.11.tar.xz'
+tar -xf php-7.2.11.tar.xz
+cd php-7.2.11
+./configure --prefix="$PREFIX" --with-libdir=lib64 --enable-fpm --with-pdo-mysql --with-mysqli --with-pdo-pgsql --with-pgsql --enable-bcmath --enable-calendar --enable-exif --enable-ftp --enable-mbstring --enable-soap --enable-zip --with-curl --with-freetype-dir --with-gd --with-gettext --with-gmp --with-iconv --with-imap-ssl --with-jpeg-dir --with-kerberos --with-ldap --with-mhash --with-openssl --with-png-dir --with-pspell --with-tidy --with-xmlrpc --with-xsl --with-zlib-dir --without-pear --enable-sockets --enable-intl --with-webp-dir --enable-pcntl --with-mysql-sock=/var/lib/mysql/mysql.sock
 make -j4
 make install
 
 ###########################################################
-# APR 1.5.2
-# original: http://apache.communilink.net/apr/apr-1.5.2.tar.bz2
+# APR 1.6.5
+# original: http://apache.communilink.net/apr/apr-1.6.5.tar.bz2
 ###########################################################
 cd "$PREFIX/src"
-wget 'http://mirror.ryansanden.com/phpfpm-7.1.4/apr-1.5.2.tar.bz2'
-tar -xf apr-1.5.2.tar.bz2
-cd apr-1.5.2
+wget 'http://mirror.ryansanden.com/phpfpm-7.2.11/apr-1.6.5.tar.bz2'
+tar -xf apr-1.6.5.tar.bz2
+cd apr-1.6.5
 ./configure --prefix="$PREFIX"
 make -j4
 make install
 
 ###########################################################
-# APR-Util 1.5.4
-# original: http://apache.communilink.net/apr/apr-util-1.5.4.tar.bz2
+# APR-Util 1.6.1
+# original: http://apache.communilink.net/apr/apr-util-1.6.1.tar.bz2
 ###########################################################
 cd "$PREFIX/src"
-wget 'http://mirror.ryansanden.com/phpfpm-7.1.4/apr-util-1.5.4.tar.bz2'
-tar -xf apr-util-1.5.4.tar.bz2
-cd apr-util-1.5.4
+wget 'http://mirror.ryansanden.com/phpfpm-7.2.11/apr-util-1.6.1.tar.bz2'
+tar -xf apr-util-1.6.1.tar.bz2
+cd apr-util-1.6.1
 ./configure --prefix="$PREFIX" --with-apr="$PREFIX"
 make -j4
 make install
 
 ###########################################################
-# Apache 2.4.25
-# original: http://apache.communilink.net//httpd/httpd-2.4.25.tar.bz2
+# Apache 2.4.37
+# original: http://apache.communilink.net/httpd/httpd-2.4.37.tar.bz2
 ###########################################################
 cd "$PREFIX/src"
-wget 'http://mirror.ryansanden.com/phpfpm-7.1.4/httpd-2.4.25.tar.bz2'
-tar -xf httpd-2.4.25.tar.bz2
-cd httpd-2.4.25
-./configure --prefix="$PREFIX" \
-            --enable-mods-shared=all \
-            --enable-mpms-shared=all \
-            --with-apr="$PREFIX" \
-            --with-apr-util="$PREFIX"
+wget 'http://mirror.ryansanden.com/phpfpm-7.2.11/httpd-2.4.37.tar.bz2'
+tar -xf httpd-2.4.37.tar.bz2
+cd httpd-2.4.37
+./configure --prefix="$PREFIX" --enable-mpms-shared=all --enable-mods-shared=all --with-apr="$PREFIX" --with-apr-util="$PREFIX"
 make -j4
 make install
 
@@ -110,7 +104,7 @@ rm -r "$PREFIX/src"
 
 #--- Create cron entry ---
 line="\n# $STACKNAME stack\n*/20 * * * * $PREFIX/bin/start"
-(crontab -l; echo -e "$line" ) | crontab -
+(crontab -l 2>/dev/null; echo -e "$line" ) | crontab -
 
 #--- Start the application ---
 $PREFIX/bin/start
